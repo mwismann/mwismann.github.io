ab69c063ca4e26dcfe687c24768bf027a11316f1
Matt <mwbertolucci@gmail.com>
2023-02-01 15:02:30 -0300

    Add work-section dynamically on page load

--- script.js
+++ script.js
@@ -71,12 +71,85 @@ const overlay = document.querySelector('#overlay');
 const header = document.querySelector('header');
 const navMenuList = document.querySelectorAll('.nav-menu');
 
-// Work-Section Variables
-const seeProjectBtn = document.querySelectorAll('.works-card-meta > a');
-const displayedProject = document.querySelector('.displayed-project-container');
-const closeProjectBtn = document.getElementById('close-project-view');
+// Works-Section Variables
+const worksSection = document.getElementById('works-section');
 
 // Functions
+const renderWorksSection = () => {
+  worksSection.insertAdjacentHTML('beforeend',
+    `<div class="container">
+      <h2>My Recent Works</h2>
+      <img src="https://raw.githubusercontent.com/mwismann/mwismann.github.io/a5668d2e5a89784262bf1747248c72134396cb1c/icon-img/indicator.svg"
+          alt="Horizontal Line Content Separator">
+      
+      <div class="works-grid-container">
+      </div>
+    </div>
+
+  <div class="displayed-project-container">
+  </div>`);
+
+  const worksGridContainer = document.querySelector('.works-grid-container');
+  const displayedProject = document.querySelector('.displayed-project-container');
+
+  projects.forEach((project) => {
+    worksGridContainer.insertAdjacentHTML('beforeend',
+      `<div class="works-grid-card">
+        <div class="works-card-meta">
+            <h3>${project.name}</h3>
+            <ul class="work-categories">
+            </ul>
+
+            <a href="#" class="action-btn" data-id="1">See Project</a>
+        </div>
+    </div>`);
+  });
+
+  const seeProjectBtn = document.querySelectorAll('.works-card-meta > a');
+
+  const displayProject = (currentBtn) => {
+    overlay.classList.add('active-alt');
+    const displayIdOf = projects.findIndex((project) => project.id === +currentBtn);
+
+    displayedProject.insertAdjacentHTML('beforeend',
+      `<div class="displayed-project">
+        <div class="project-snapshot-container">
+        <img id="snapshot-img" src="${window.innerWidth < 768 ? projects[displayIdOf]['mobile image'] : projects[displayIdOf]['desktop image']}" alt="Three Towers standing tall">
+        <img id="close-project-view" src="${window.innerWidth < 768 ? `
+        https://raw.githubusercontent.com/mwismann/mwismann.github.io/d6dfbe842fd5e8f029bb39ea9d353585a9346d0f/icon-img/project-close.svg` : `
+        https://raw.githubusercontent.com/mwismann/mwismann.github.io/d6dfbe842fd5e8f029bb39ea9d353585a9346d0f/icon-img/desktop/project-close-desk.svg`}" alt="An X to close the project view">
+        </div>
+        
+        <h3 class="project-name">${projects[displayIdOf].name}</h3>
+        <ul class="work-categories alt">
+        <li>Ruby on rails</li>
+        <li>css</li>
+        <li>JavaScript</li>
+        </ul>
+        
+        <p class="project-description">
+        ${projects[displayIdOf].description}
+        </p>
+        
+        <div class="project-action-btn">
+        <a href="${projects[displayIdOf].liveVersion}" class="action-btn live-btn">See Live <img src="icon-img/icon-live.svg" alt="Arrow Pointing To Upper Right Corner"></a>
+        <a href="${projects[displayIdOf].source}" class="action-btn source-btn">See Source <img src="icon-img/github-white.svg" alt="GitHub logo"></a>
+        </div>
+        </div>`);
+
+    const closeProjectBtn = document.getElementById('close-project-view');
+    const closeProject = () => {
+      overlay.classList.remove('active-alt');
+      displayedProject.innerHTML = '';
+    };
+    closeProjectBtn.addEventListener('click', closeProject);
+  };
+
+  seeProjectBtn.forEach((btn) => btn.addEventListener('click', (e) => {
+    displayProject(e.target.dataset.id);
+  }));
+};
+
 const openMenu = () => {
   logoContainer.classList.add('hidden');
   hamburger.classList.add('hidden');
@@ -97,47 +170,8 @@ const closeMenu = () => {
   header.classList.remove('active');
 };
 
-const displayProject = (currentBtn) => {
-  overlay.classList.add('active-alt');
-  const displayIdOf = projects.findIndex((project) => project.id === +currentBtn);
-
-  displayedProject.insertAdjacentHTML('beforeend',
-    `<div class="displayed-project">
-      <div class="project-snapshot-container">
-      <img id="snapshot-img" src="${window.innerWidth < 768 ? projects[displayIdOf]['mobile image'] : projects[displayIdOf]['desktop image']}" alt="Three Towers standing tall">
-      <img id="close-project-view" src="${window.innerWidth < 768 ? `
-      https://raw.githubusercontent.com/mwismann/mwismann.github.io/d6dfbe842fd5e8f029bb39ea9d353585a9346d0f/icon-img/project-close.svg` : `
-      https://raw.githubusercontent.com/mwismann/mwismann.github.io/d6dfbe842fd5e8f029bb39ea9d353585a9346d0f/icon-img/desktop/project-close-desk.svg`}" alt="An X to close the project view" onclick="closeProject()">
-      </div>
-      
-      <h3 class="project-name">${projects[displayIdOf].name}</h3>
-      <ul class="work-categories alt">
-      <li>Ruby on rails</li>
-      <li>css</li>
-      <li>JavaScript</li>
-      </ul>
-      
-      <p class="project-description">
-      ${projects[displayIdOf].description}
-      </p>
-      
-      <div class="project-action-btn">
-      <a href="${projects[displayIdOf].liveVersion}" class="action-btn live-btn">See Live <img src="icon-img/icon-live.svg" alt="Arrow Pointing To Upper Right Corner"></a>
-      <a href="${projects[displayIdOf].source}" class="action-btn source-btn">See Source <img src="icon-img/github-white.svg" alt="GitHub logo"></a>
-      </div>
-      </div>`);
-};
-
-const closeProject = () => {
-  overlay.classList.remove('active-alt');
-  displayedProject.innerHTML = '';
-};
-
-// eventListeners
+// eventListeners and Functions Calls
+window.onload = renderWorksSection;
 hamburger.addEventListener('click', openMenu);
 closeMobileMenu.addEventListener('click', closeMenu);
 navMenuList.forEach((element) => element.addEventListener('click', closeMenu));
-seeProjectBtn.forEach((btn) => btn.addEventListener('click', (e) => {
-  displayProject(e.target.dataset.id);
-}));
-closeProjectBtn.addEventListener('click', closeProject);
